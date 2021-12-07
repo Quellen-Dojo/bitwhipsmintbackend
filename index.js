6fe305747796d38022a5920215733b29661b0c32
Quellen-Dojo <marquelle.nesbitt@gmail.com>
2021-12-07 11:02:33 -0800

    add airdrop support

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ const WhitelistSchema = new Schema({
     wallet: String
 });
 
-const WhitelistSeries1 = mongoose.model('Whitelist', WhitelistSchema)
+const WhitelistSeries1 = mongoose.model('Whitelist', WhitelistSchema);
+const AirdropsSeries1 = mongoose.model('AirdropS1', WhitelistSchema);
 
 function validateWallet(wallet) {
     //In base58, there is no 0, O, l, or I in the wallet string.
@@ -53,6 +54,19 @@ async function walletInWhitelist(wallet) {
     return (await WhitelistSeries1.findOne({ wallet: wallet }).exec()) !== null;
 }
 
+//Check if the wallet exists inside the airdrop
+async function walletInAirdrop(wallet) {
+    return (await AirdropsSeries1.findOne({ wallet: wallet }).exec()) !== null;
+}
+
+/**
+ * Huh?
+ * @param {mongoose.Model} model 
+ */
+async function getNumberInModel(model) {
+    return await model.estimatedDocumentCount().exec();
+}
+
 app.get('/checkwhitelist', async (req, res) => {
     const { wallet, key } = req.query;
     if (checkingWhitelist) {
@@ -64,6 +78,35 @@ app.get('/checkwhitelist', async (req, res) => {
         }
     } else {
         res.status(200).send();
+    }
+});
+
+app.get('/checkairdrop', async (req, res) => {
+    const { wallet, key } = req.query;
+    if (checkingWhitelist) {
+        if (await walletInAirdrop(wallet)) {
+            //Exists in whitelist
+            res.status(200).send();
+        } else {
+            res.status(404).send();
+        }
+    } else {
+        res.status(200).send();
+    }
+});
+
+app.get('/getstats', async (req, res) => {
+    const { key } = req.query;
+    if (key == currentKey) {
+        try {
+            const numWhitelists = await getNumberInModel(WhitelistSeries1);
+            const numAirdrops = await getNumberInModel(AirdropsSeries1);
+            res.json({ whitelists: numWhitelists, airdrops: numAirdrops }).send();
+        } catch (e) {
+            res.status(500).send();
+        }
+    } else {
+        res.status(401).send();
     }
 });
 
@@ -81,6 +124,26 @@ app.post('/addtowhitelist', async (req, res) => {
             if (!(await walletInWhitelist(wallet))) {
                 try {
                     await WhitelistSeries1.create({ wallet: wallet, series: 1 });
+                    res.status(200).send();
+                } catch (e) {
+                    res.status(500).send();
+                }
+            } else {
+                res.status(409).send();
+            }
+        }
+    } else {
+        res.status(401).send();
+    }
+});
+
+app.post('/addtoairdrop', async (req, res) => {
+    const { key, wallet, list } = req.body;
+    if (key == currentKey) {
+        if (wallet) {
+            if (!(await walletInAirdrop(wallet))) {
+                try {
+                    await AirdropsSeries1.create({ wallet: wallet, series: 1 });
                     res.status(200).send();
                 } catch (e) {
                     res.status(500).send();

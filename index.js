e5d4850e0932e14f57e54c5c982d97f33c613ea0
Quellen-Dojo <marquelle.nesbitt@gmail.com>
2021-12-30 19:29:56 -0800

    add /gets1airdrop

--- index.js
+++ index.js
@@ -96,6 +96,21 @@ app.get('/gets1whitelist', async (req, res) => {
     }
 });
 
+app.get('/gets1airdrop', async (req, res) => {
+    const { key } = req.query;
+    if (key == currentKey) {
+        AirdropsSeries1.find((err, doc) => {
+            if (err) {
+                res.status(500).send();
+            } else {
+                console.log(doc);
+                res.json(doc.map((v, i) => v.wallet)).send();
+                res.status(200).send();
+            }
+        });
+    }
+});
+
 app.get('/checkairdrop', async (req, res) => {
     const { wallet, key } = req.query;
     if (checkingWhitelist) {
